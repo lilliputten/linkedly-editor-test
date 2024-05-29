d4388463d7fb5cc6ee2372fa74e903bb2c94245f
lilliputten <lilliputten@gmail.com>
2024-05-30 00:47:46 +0500

    Issue #21: Minor changes.

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,14 +1,6 @@
 import { RouteObject } from 'react-router-dom';
 
-import { AppRootLayout } from 'src/pages/app/AppRootLayout';
-import { DemoPage } from 'src/pages/app/DemoPage';
-import { LoginPage } from 'src/pages/app/LoginPage';
-import { StartPage } from 'src/pages/app/StartPage';
-import { TestPage } from 'src/pages/app/TestPage';
-import { WaitingPage } from 'src/pages/app/WaitingPage';
-import { EditSurveyPage } from 'src/pages/main/EditSurveyPage';
-import { MainLayout } from 'src/pages/main/MainLayout';
-import { MainSurveysList } from 'src/pages/main/MainSurveysList';
+import { makeUrl } from 'src/core/helpers/urls';
 
 import {
   demoRoute,
@@ -19,6 +11,16 @@ import {
   startRoute,
   testRoute,
 } from './appUrls';
+
+import { AppRootLayout } from 'src/pages/app/AppRootLayout';
+import { DemoPage } from 'src/pages/app/DemoPage';
+import { EditSurveyPage } from 'src/pages/main/EditSurveyPage';
+import { LoginPage } from 'src/pages/app/LoginPage';
+import { MainLayout } from 'src/pages/main/MainLayout';
+import { MainSurveysList } from 'src/pages/main/MainSurveysList';
+import { StartPage } from 'src/pages/app/StartPage';
+import { TestPage } from 'src/pages/app/TestPage';
+import { WaitingPage } from 'src/pages/app/WaitingPage';
 
 /** Routes
  * @see src/routes/appUrls.ts
@@ -38,16 +40,13 @@ export const routes: RouteObject[] = [
         children: [
           // prettier-ignore
           { index: true, element: <MainSurveysList /> },
-          { path: mainSurveyRoute + '/:surveyId', element: <EditSurveyPage /> },
+          { path: makeUrl([mainSurveyRoute, ':surveyId']), element: <EditSurveyPage /> },
           { path: startRoute, element: <TestPage /> },
         ],
       },
-      { path: demoRoute, element: <DemoPage /> },
-      { path: testRoute, element: <TestPage /> },
-      { path: startRoute, element: <StartPage /> },
+      { path: startRoute, element: <StartPage /> }, // TODO?
+      { path: demoRoute, element: <DemoPage /> }, // NOTE: Debug only!
+      { path: testRoute, element: <TestPage /> }, // NOTE: Debug only!
     ],
   },
-  // TODO:
-  // - finished
-  // - ready?
 ];
