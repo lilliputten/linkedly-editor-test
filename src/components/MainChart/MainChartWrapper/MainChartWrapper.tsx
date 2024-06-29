ea27e99ae68bd15d4286d544d04e8ba865c2af58
lilliputten <lilliputten@gmail.com>
2024-06-29 10:54:51 +0500

    Issue #7: Added drag and drop for seqeunce items.

--- src/components/MainChart/MainChartWrapper/MainChartWrapper.tsx
+++ src/components/MainChart/MainChartWrapper/MainChartWrapper.tsx
@@ -5,6 +5,8 @@ import { Box, Stack, Typography } from '@mui/material';
 
 import { TPropsWithClassName } from 'src/core/types';
 import { useAppDataStore } from 'src/store/AppDataStore';
+import { dragSequenceItemFormatId } from 'src/entities/Sequence/constants';
+import { TSequenceItem } from 'src/entities/Sequence/types';
 
 export interface TMainChartWrapperProps extends TPropsWithClassName {
   // handleChange?: (params: TSequenceNodeChangeParams) => void;
@@ -17,8 +19,60 @@ export const MainChartWrapper: React.FC<TMainChartWrapperProps> = observer((prop
   const appDataStore = useAppDataStore();
   const { sequenceData, hasSequenceDataChanged } = appDataStore;
   const hasData = !!sequenceData;
+  const onDragOver = React.useCallback((event: any) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
+  const onDrop = React.useCallback((event: any) => {
+    event.preventDefault();
+    const dataJson = event.dataTransfer.getData(dragSequenceItemFormatId);
+    const itemData = dataJson ? (JSON.parse(dataJson) as TSequenceItem) : undefined;
+
+    console.log('[MainChartWrapper:onDrop]', {
+      itemData,
+    });
+
+    // Check if the dropped element is valid
+    if (!itemData?.itemId) {
+      return;
+    }
+
+    /* // TODO: Add new node to a chart
+     * // reactFlowInstance.project was renamed to reactFlowInstance.screenToFlowPosition
+     * // and you don't need to subtract the reactFlowBounds.left/top anymore
+     * // details: https://reactflow.dev/whats-new/2023-11-10
+     * // @ts-ignore
+     * const position = reactFlowInstance.screenToFlowPosition({
+     *   x: event.clientX,
+     *   y: event.clientY,
+     * });
+     * const newNode = {
+     *   id: getId(),
+     *   type: data.type,
+     *   position,
+     *   data: {
+     *     state: {
+     *       task: data.name,
+     *       transitions: {
+     *         success: "end",
+     *         fail: "end"
+     *       }
+     *     },
+     *     campaignStatus: campaign.status,
+     *     label: data.label,
+     *   }
+     * };
+     * setNodes((nds) => { return nds.concat(newNode) });
+     */
+  }, []);
+
   return (
-    <Stack className={classNames(className, 'MainChartWrapper')}>
+    <Stack
+      // prettier-ignore
+      className={classNames(className, 'MainChartWrapper')}
+      onDrop={onDrop}
+      onDragOver={onDragOver}
+    >
       {hasData && (
         <>
           {hasSequenceDataChanged && (
@@ -29,7 +83,7 @@ export const MainChartWrapper: React.FC<TMainChartWrapperProps> = observer((prop
               </Typography>
             </Box>
           )}
-          <>Main content</>
+          <>Main chart</>
         </>
       )}
     </Stack>
--- src/entities/Sequence/constants/index.ts
+++ src/entities/Sequence/constants/index.ts
@@ -1,1 +1,2 @@
 export * from './sequenceItemTypeNames';
+export * from './dragSequenceItem';
